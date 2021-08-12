d1c65c6590f5f5b3ad8db18216fa6f8985e03efc
Oluwole <oluwole@Oluwoles-MacBook-Pro.local>
2021-08-12 05:03:16 +0100

    --amend

--- src/controllers/hobbies.ts
+++ src/controllers/hobbies.ts
@@ -3,15 +3,15 @@ import { responseHelper } from '../helpers';
 import { Request, Response } from 'express';
 
   /**
-     * @description create question 
+     * @description create hobbies 
      * @param {Object} req - Http Request object
      * @param {Object} res - Http Request object
      * @returns {Object} returns object of the required response
      */
 class HobbiesController {
+
   async createHobbies(req: Request, res: Response) {
     try {
-      // We attach the code validation error caused by bad request
       const hobbies = await hobbiesServices.createHobbies(req);
       return responseHelper.successResponse(res, { hobbies });
     } catch (error) {
@@ -20,8 +20,16 @@ class HobbiesController {
       return responseHelper.errorResponse(res, error, { hobbies: [] });
     }
   }
+
+  /**
+     * @description updates hobbies 
+     * @param {Object} req - Http Request object
+     * @param {Object} res - Http Request object
+     * @returns {Object} returns object of the required response
+     */
   async updateHobbies(req: Request, res: Response) {
     try {
+      //update hobbies
       const hobbies = await hobbiesServices.updateHobbies(req);
       return responseHelper.successResponse(res, { hobbies });
     } catch (error) {
@@ -31,8 +39,16 @@ class HobbiesController {
     }
   }
 
+  /**
+     * @description get hobbies 
+     * @param {Object} req - Http Request object
+     * @param {Object} res - Http Request object
+     * @returns {Object} returns object of the required response
+     */
+
   async fetchHobbies(req: Request, res: Response) {
     try {
+      //gets hobbies
       const hobbies = await hobbiesServices.fetchHobbies(req);
       return responseHelper.successResponse(res, { hobbies });
     } catch (error) {
@@ -41,8 +57,17 @@ class HobbiesController {
       return responseHelper.errorResponse(res, error, { hobbies: [] });
     }
   }
+
+  /**
+     * @description delete hobbies 
+     * @param {Object} req - Http Request object
+     * @param {Object} res - Http Request object
+     * @returns {Object} returns object of the required response
+     */
+
   async deleteHobbies(req: Request, res: Response) {
     try {
+      //deletes hobbies
       const hobbies = await hobbiesServices.deleteHobbies(req);
       return responseHelper.successResponse(res, { hobbies });
     } catch (error) {
