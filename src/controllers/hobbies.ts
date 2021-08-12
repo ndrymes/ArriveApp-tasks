2722e04488db310d23804fcf76d23326fac351ff
Oluwole <oluwole@Oluwoles-MacBook-Pro.local>
2021-08-12 04:57:31 +0100

    added error log helper to response

--- src/controllers/hobbies.ts
+++ src/controllers/hobbies.ts
@@ -1,9 +1,17 @@
 import { hobbiesServices } from '../services/hobbies';
 import { responseHelper } from '../helpers';
 import { Request, Response } from 'express';
+
+  /**
+     * @description create question 
+     * @param {Object} req - Http Request object
+     * @param {Object} res - Http Request object
+     * @returns {Object} returns object of the required response
+     */
 class HobbiesController {
   async createHobbies(req: Request, res: Response) {
     try {
+      // We attach the code validation error caused by bad request
       const hobbies = await hobbiesServices.createHobbies(req);
       return responseHelper.successResponse(res, { hobbies });
     } catch (error) {
@@ -17,6 +25,8 @@ class HobbiesController {
       const hobbies = await hobbiesServices.updateHobbies(req);
       return responseHelper.successResponse(res, { hobbies });
     } catch (error) {
+      // We attach the code validation error caused by bad request
+      error.code = responseHelper.responseCodes.badRequest;
       return responseHelper.errorResponse(res, error, { hobbies: [] });
     }
   }
@@ -26,6 +36,8 @@ class HobbiesController {
       const hobbies = await hobbiesServices.fetchHobbies(req);
       return responseHelper.successResponse(res, { hobbies });
     } catch (error) {
+      // We attach the code validation error caused by bad request
+      error.code = responseHelper.responseCodes.badRequest;
       return responseHelper.errorResponse(res, error, { hobbies: [] });
     }
   }
@@ -34,6 +46,8 @@ class HobbiesController {
       const hobbies = await hobbiesServices.deleteHobbies(req);
       return responseHelper.successResponse(res, { hobbies });
     } catch (error) {
+      // We attach the code validation error caused by bad request
+      error.code = responseHelper.responseCodes.badRequest;
       return responseHelper.errorResponse(res, error, { hobbies: [] });
     }
   }
